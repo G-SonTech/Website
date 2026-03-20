db49342a71ac91bd80819a1f5623d3e35d7f5191
sebgeson <sebbelol95@gmail.com>
2026-03-20 10:58:33 +0100

    fix again

--- functions/main.py
+++ functions/main.py
@@ -6,6 +6,7 @@ sys.path.insert(0, str(Path(__file__).parent.parent))
 from app import app
 import serverless_wsgi
 
+
 def handler(event, context):
-    """Netlify Functions handler for Flask app"""
+    """Netlify Functions handler for Flask app."""
     return serverless_wsgi.handle_request(app, event, context)
